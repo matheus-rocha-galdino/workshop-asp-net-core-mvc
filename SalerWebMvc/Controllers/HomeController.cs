bcd2ee91d2bcd54383d1f7ee250fa311bbeed8b7
mathrochaa <54423058+mathrochaa@users.noreply.github.com>
2020-01-16 19:19:19 -0300

    First controller and Razor page tests

--- SalerWebMvc/Controllers/HomeController.cs
+++ SalerWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalerWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData ["Professor"] = "Matheus Rocha";
 
             return View();
         }
